72e800a64239cccbc12a1d23f2d7f55359ba509a
perfumelim <perfumelim@gmail.com>
2022-05-01 21:20:54 +0900

    05/01

---
+++ 가위_바위_보.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+  let answer = "";
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === b[i]) answer += "D" + " ";
+  }
+  return answer;
+}
+
+let a = [2, 3, 3, 1, 3];
+let b = [1, 1, 2, 2, 3];
+console.log(solution(a, b));
